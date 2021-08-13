93c78f251551d5179b8cef92160591f3fd089e71
Yasser Mohamed <yassermano848@gmail.com>
2021-08-13 15:19:27 +0200

    Add files via upload

---
+++ 2_last_digit_of_fibonacci_number/cmake-build-debug/CMakeFiles/2_last_digit_of_fibonacci_number.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "2_last_digit_of_fibonacci_number"
+  "2_last_digit_of_fibonacci_number.pdb"
+  "CMakeFiles/2_last_digit_of_fibonacci_number.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_last_digit_of_fibonacci_number.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
