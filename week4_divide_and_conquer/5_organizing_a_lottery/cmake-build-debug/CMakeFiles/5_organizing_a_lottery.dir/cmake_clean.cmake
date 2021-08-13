53bd3019e03f75dd2ec73613d88a1d16f1c47198
Yasser Mohamed <yassermano848@gmail.com>
2021-08-13 15:56:50 +0200

    Add files via upload

---
+++ week4_divide_and_conquer/5_organizing_a_lottery/cmake-build-debug/CMakeFiles/5_organizing_a_lottery.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "5_organizing_a_lottery"
+  "5_organizing_a_lottery.pdb"
+  "CMakeFiles/5_organizing_a_lottery.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/5_organizing_a_lottery.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
