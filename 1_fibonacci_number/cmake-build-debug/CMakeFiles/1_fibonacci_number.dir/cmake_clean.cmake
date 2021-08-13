5b08ef03d56dd11535ad03b1d7d33b26493d50c9
Yasser Mohamed <yassermano848@gmail.com>
2021-08-13 15:14:40 +0200

    Add files via upload

---
+++ 1_fibonacci_number/cmake-build-debug/CMakeFiles/1_fibonacci_number.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "1_fibonacci_number"
+  "1_fibonacci_number.pdb"
+  "CMakeFiles/1_fibonacci_number.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_fibonacci_number.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
